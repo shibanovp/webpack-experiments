31859a86ca67e3b9358ea2d563a2e6b4034a86c5
Paul <paul.shibanov@gmail.com>
2016-05-18 13:19:32 +0400

    Added minify for production

--- webpack.config.js
+++ webpack.config.js
@@ -26,3 +26,15 @@ module.exports = {
         }]
     }
 }
+
+if (NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    )
+}
